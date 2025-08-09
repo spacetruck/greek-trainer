6f3089ca71b9ef08c8806f7e4e89bb66ee8a311c
spacetruck <politicsks@outlook.com>
2025-08-08 22:49:06 -0700

    updates for service worker cache refresh

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 
-const CACHE_NAME = "greek-trainer-cache-v2";
-const URLS_TO_CACHE = ["./","./index.html","./manifest.json","./service-worker.js","./icon-192.png","./icon-512.png"];
-self.addEventListener("install", e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLS_TO_CACHE))); });
-self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))).then(()=>self.clients.claim())); });
-self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
+const CACHE_NAME="greek-trainer-cache-v3";
+const URLS_TO_CACHE=["./","./index.html","./manifest.json","./service-worker.js","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLS_TO_CACHE)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
