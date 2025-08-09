1a9a5dddf8abe3fb1c665611240f34ac9c9761e9
spacetruck <politicsks@outlook.com>
2025-08-08 23:43:30 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME="greek-trainer-cache-v4";
+const CACHE_NAME="greek-trainer-cache-v5";
 const URLS_TO_CACHE=["./","./index.html","./manifest.json","./service-worker.js","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLS_TO_CACHE)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k)))).then(()=>self.clients.claim()))});
