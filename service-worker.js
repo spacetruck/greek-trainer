1cd64d452d233c41301edc5995bfcda059ba66e5
spacetruck <politicsks@outlook.com>
2025-08-08 21:32:18 -0700

    updated a few bugs and added state quiz
    minor improvements

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,6 @@
 
-const CACHE_NAME = "greek-trainer-cache-v1";
-const URLS_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./service-worker.js",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+const CACHE_NAME = "greek-trainer-cache-v2";
+const URLS_TO_CACHE = ["./","./index.html","./manifest.json","./service-worker.js","./icon-192.png","./icon-512.png"];
+self.addEventListener("install", e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLS_TO_CACHE))); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
